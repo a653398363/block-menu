84e5df542e23049c2f31a354122ba187fa6721b8
fanqie <fan.liu@kedalo.com>
2020-05-21 18:09:10 +0800

    add npm

---
+++ src/components/block-menu.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    block-menu
+    block-menu
+    <ul>
+      <li v-for="i in menuArr" :key="i.id">{{i.value}}</li>
+    </ul>
+  </div>
+</template>
+<script>
+export default {
+  name: 'block-menu',
+  props: {
+    menuArr: {
+      type: Array,
+      default () {
+        return []
+      }
+    }
+  },
+  data () {
+    return {
+    }
+  }
+}
+</script>
--- src/components/index.js
+++ src/components/index.js
@@ -1,7 +1,7 @@
 // src/index.js
 // 这里import 的 btn和btnDemo.vue的name属性名相同 ！
 
-import blockMenu from './components/block-menu.vue'
+import blockMenu from './block-meun.vue'
 
 blockMenu.install = Vue => Vue.component(blockMenu.name, blockMenu) // 给组件配置install方法 
 
